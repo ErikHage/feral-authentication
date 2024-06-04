90e0557e0c5d76c5cb137e75d4e47eb31d86b18b
Erik Hage <ehage4@gmail.com>
2024-06-04 19:14:29 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <FeralAuthenticationApp/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import FeralAuthenticationApp from "@/components/FeralAuthenticationApp.vue";
+
+export default {
+  name: 'App',
+
+  components: {
+    FeralAuthenticationApp,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/FeralAuthenticationApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data: () => ({
+
+  }),
+  computed: {
+
+  },
+  methods: {
+
+  },
+  name: 'VigilantHarvestApp',
+}
+</script>
