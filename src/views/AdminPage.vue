ed9466a62c5ad4dcf727b075ff62388e9647412d
Erik Hage <ehage4@gmail.com>
2024-06-06 16:36:20 -0400

    add admin page skeleton

---
+++ src/views/AdminPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <h1>Admin Page</h1>
+    <v-row class="text-center">
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'AdminPage',
+
+  methods: {
+
+  },
+}
+</script>
