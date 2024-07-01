5859289b7e3654278cbbf4d40d1ce772567d4810
Erik Hage <ehage4@gmail.com>
2024-06-30 23:14:27 -0400

    remove unused code

--- src/views/admin/AuthorizationAdmin.vue
+++ src/views/admin/AuthorizationAdmin.vue
@@ -159,12 +159,5 @@ export default {
 </script>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 1s;
-}
 
-.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */
-{
-  opacity: 0;
-}
 </style>
