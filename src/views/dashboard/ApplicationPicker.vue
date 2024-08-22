6d98e20b699d2d57b4b6883a4951dd0b953c793b
Erik Hage <ehage4@gmail.com>
2024-08-22 00:31:51 -0400

    move assets to public bc they are dropped by vite build (not explicitly used)

--- src/views/dashboard/ApplicationPicker.vue
+++ src/views/dashboard/ApplicationPicker.vue
@@ -66,9 +66,9 @@ export default {
 
     getAssetPathToDisplay(application) {
       if (application.mascotAssetPath) {
-        return `/src/assets/${application.mascotAssetPath}`;
+        return `/assets/${application.mascotAssetPath}`;
       }
-      return '/src/assets/default-mascot.jpg';
+      return '/assets/default-mascot.jpg';
     },
 
     async goToApplication(applicationId) {
